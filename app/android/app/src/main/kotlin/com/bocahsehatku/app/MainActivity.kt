622cd62a6b99447bdf2c6ac90c19715b7e5cb4bf
Putraaa <61768347+vickycxc@users.noreply.github.com>
2025-07-15 10:30:46 +0700

    first commit

---
+++ app/android/app/src/main/kotlin/com/bocahsehatku/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bocahsehatku.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+const app = express();
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
